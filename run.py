76f2f0e2cbcb22878394365c19a5d43cd7f59700
Paulina Dolder <paulid4628@gmail.com>
2025-02-27 21:44:32 +0000

    Basic input structure tested

--- run.py
+++ run.py
@@ -10,3 +10,16 @@ print(Fore.CYAN + "Welcome to")
 print(Back.LIGHTBLUE_EX + "RepayRite:")
 print(Style.DIM + "Mortgage Repayment")
 print(Style.RESET_ALL + "Calculator!")
+
+"""
+Initial user input that will provide figures for the mortgage repayment formula to be used
+"""
+
+loan_amount = float(input("Please enter your full loan amount:"))
+print(loan_amount)
+
+annual_interest_rate = float(input("Please enter your annual interest rate:"))
+print(annual_interest_rate)
+
+loan_term = int(input("Please enter your loan term in years:"))
+print(loan_term)
